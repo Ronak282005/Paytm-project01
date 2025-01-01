37379dfef56e129cb08a8fcfb517a5aa8c2d0f7a
ronak <ronakpraja340@gmail.com>
2025-01-01 18:10:03 +0530

    added backend logic for fontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes,Route } from "react-router-dom"
 import {Signup} from "./pages/Signup.jsx"
 import {Signin} from "./pages/Signin.jsx"
 import {Dashboard} from "./pages/Dashboard.jsx"
-// import SendMoney from "./pages/SendMoney"
+import {SendMoney} from "./pages/SendMoney"
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      {/* <Route path="/send" element={<SendMoney />} /> */}
+      <Route path="/send" element={<SendMoney />} />
     </Routes>
     </BrowserRouter>
   )
